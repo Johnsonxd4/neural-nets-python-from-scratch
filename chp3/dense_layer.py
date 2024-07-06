ed130626c79ade88211516d825f9a3ec4bedd54c
Paulo Henrique dos Reis Moura <paulohenriquedosreismoura@gmail.com>
2024-07-06 17:40:33 -0300

    Chapter 4

---
+++ chp3/dense_layer.py
@@ -0,0 +1,18 @@
+import nnfs
+nnfs.init()
+
+from nnfs.datasets import spiral_data
+import numpy as np
+class Layer_Dense:
+    def __init__(self, n_inputs,n_newrons):
+        self.weights = 0.01 * np.random.randn(n_inputs,n_newrons)
+        self.biases = np.zeros((1, n_newrons))
+        
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.biases
+
+X,y = spiral_data(samples=100,classes=3)
+
+dense1 = Layer_Dense(2,3)
+dense1.forward(X)
+print(dense1.output)
--- chp4/ReLU-activation.py
+++ chp4/ReLU-activation.py
@@ -1,8 +1,8 @@
 import nnfs
 nnfs.init()
-
 from nnfs.datasets import spiral_data
 import numpy as np
+
 class Layer_Dense:
     def __init__(self, n_inputs,n_newrons):
         self.weights = 0.01 * np.random.randn(n_inputs,n_newrons)
@@ -11,8 +11,13 @@ class Layer_Dense:
     def forward(self,inputs):
         self.output = np.dot(inputs,self.weights) + self.biases
 
-X,y = spiral_data(samples=100,classes=3)
+class Activation_ReLU:
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
 
+X,y = spiral_data(samples=100, classes=3)
 dense1 = Layer_Dense(2,3)
+activation = Activation_ReLU()
 dense1.forward(X)
-print(dense1.output[:5])
+activation.forward(dense1.output)
+print(activation.output[:5])
