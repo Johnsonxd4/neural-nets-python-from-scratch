91f095dfd5fb15988c17641addb2d0e58c2cf5ae
Paulo Henrique dos Reis Moura <paulohenriquedosreismoura@gmail.com>
2024-07-06 20:22:02 -0300

    Chapter 5

--- chp5/wrapping-up.py
+++ chp5/wrapping-up.py
@@ -60,9 +60,12 @@ class Loss_CategoricalCrossentropy(Loss):
 X, y = spiral_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
+
 dense2 = Layer_Dense(3, 3)
 activation2 = Activation_Softmax()
+
 loss_function = Loss_CategoricalCrossentropy()
+
 dense1.forward(X)
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
@@ -70,3 +73,8 @@ activation2.forward(dense2.output)
 print(activation2.output[:5])
 loss = loss_function.calculate(activation2.output, y)
 print('loss:', loss)
+predictions = np.argmax(activation2.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+print('acc:', accuracy)
